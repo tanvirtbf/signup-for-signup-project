996fa44bd5f5258b49eddd0584ed3f35b3e2a96b
tanvirtbf <tanver018765@gmail.com>
2024-09-30 09:35:09 +0600

    form redesign

--- my-app/src/app/signup/page.jsx
+++ my-app/src/app/signup/page.jsx
@@ -8,23 +8,26 @@ const Signup = () => {
         <form action="" className="w-full flex flex-col gap-3">
           <div className="flex flex-col gap-1">
             <label htmlFor="firstName">First Name</label>
-            <input className="p-3 rounded-md" type="text" id="firstName" placeholder="Type Your First Name " />
+            <input className="border outline-none p-3 rounded-md" type="text" id="firstName" placeholder="Type Your First Name " />
           </div>
           <div className="flex flex-col gap-1">
             <label htmlFor="lastName">Last Name</label>
-            <input className="p-3 rounded-md" type="text" id="lastName" placeholder="Type Your Last Name " />
+            <input className="border outline-none p-3 rounded-md" type="text" id="lastName" placeholder="Type Your Last Name " />
           </div>
           <div className="flex flex-col gap-1">
             <label htmlFor="userName">User Name</label>
-            <input className="p-3 rounded-md" type="text" id="userName" placeholder="Type Your User Name " />
+            <input className="border outline-none p-3 rounded-md" type="text" id="userName" placeholder="Type Your User Name " />
           </div>
           <div className="flex flex-col gap-1">
             <label htmlFor="email">Email</label>
-            <input className="p-3 rounded-md" type="text" id="email" placeholder="Type Your Email " />
+            <input className="border outline-none p-3 rounded-md" type="text" id="email" placeholder="Type Your Email " />
           </div>
           <div className="flex flex-col gap-1">
             <label htmlFor="password">Password</label>
-            <input className="p-3 rounded-md" type="password" id="password" placeholder="Set a New Password" />
+            <input className="border outline-none p-3 rounded-md" type="password" id="password" placeholder="Set a New Password" />
+          </div>
+          <div>
+            <button className="bg-blue-300 py-3 w-full rounded-lg hover:bg-red-200 font-medium">Submit</button>
           </div>
         </form>
       </div>
