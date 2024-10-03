4bb4cf7a8a79810a123c37036c2d9ef18f068a55
tanvirtbf <tanver018765@gmail.com>
2024-10-03 17:49:15 +0600

    user icon placed

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 "use client"
+import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import UserIcon from '../../public/user.png'
 import React from 'react'
 
 const Navbar = () => {
@@ -14,10 +16,10 @@ const Navbar = () => {
 
       <div className="w-full h-full flex justify-between items-center">
 
-        <div className="">
+        <div className="flex items-center">
           <h1 className="font-bold text-black text-3xl cursor-pointer" onClick={()=> router.push('/')}>ADSTERRAA</h1>
         </div>
-        <div className="flex gap-4 h-full">
+        <div className="flex gap-4 h-full items-center">
           <div className='flex justify-center items-center h-full'>
             <Link className="font-bold text-black text-xl" href="/">Home</Link>
           </div>
@@ -31,9 +33,17 @@ const Navbar = () => {
             <Link className="font-bold text-black text-xl" href="/">Contact Us</Link>
           </div>
         </div>
-        <div className="flex gap-3 ">
-          <Link href="/login" className="px-4 py-2 bg-[#7B3F00] rounded-md text-white font-medium text-center">Login</Link>
-          <Link href="/signup" className="px-4 py-2 bg-[#7B3F00] rounded-md text-white font-medium text-center">SignUp</Link>
+        <div className="flex gap-3 items-center">
+          <div>
+            <Link href="/login" className="px-4 py-2 bg-[#7B3F00] rounded-md text-white font-medium text-center">Login</Link>
+          </div>
+          <div>
+            <Link href="/signup" className="px-4 py-2 bg-[#7B3F00] rounded-md text-white font-medium text-center">SignUp</Link>
+          </div>
+          <div className='w-12 h-12 flex items-center ml-2'>
+            <Image className='w-full h-full rounded-[50%]' src={UserIcon} />
+          </div>
+
         </div>
 
       </div>
