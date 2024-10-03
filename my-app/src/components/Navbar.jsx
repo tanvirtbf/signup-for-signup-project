9ca7c7803b9daa86c4dd514742577b59fdf07a8b
tanvirtbf <tanver018765@gmail.com>
2024-10-03 17:29:22 +0600

    navbar cpmplete

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,7 +1,11 @@
+"use client"
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const Navbar = () => {
+
+  const router = useRouter()
 
   const clsName = ``
 
@@ -11,13 +15,21 @@ const Navbar = () => {
       <div className="w-full h-full flex justify-between items-center">
 
         <div className="">
-          <h1 className="font-bold text-black text-3xl">ADSTERRAA</h1>
+          <h1 className="font-bold text-black text-3xl cursor-pointer" onClick={()=> router.push('/')}>ADSTERRAA</h1>
         </div>
-        <div className="flex gap-2 h-full">
-          <Link className="bg-slate-400  h-full w- font-bold text-black text-2xl" href="/">Home</Link>
-          <Link className="bg-slate-400  h-full w- font-bold text-black text-2xl" href="/">About</Link>
-          <Link className="bg-slate-400  h-full w- font-bold text-black text-2xl" href="/">Services</Link>
-          <Link className="bg-slate-400  h-full w- font-bold text-black text-2xl" href="/">Contact Us</Link>
+        <div className="flex gap-4 h-full">
+          <div className='flex justify-center items-center h-full'>
+            <Link className="font-bold text-black text-xl" href="/">Home</Link>
+          </div>
+          <div className='flex justify-center items-center h-full'>
+            <Link className="font-bold text-black text-xl" href="/">About</Link>
+          </div>
+          <div className='flex justify-center items-center h-full'>
+            <Link className=" font-bold text-black text-xl" href="/">Services</Link>
+          </div>
+          <div className='flex justify-center items-center h-full'>
+            <Link className="font-bold text-black text-xl" href="/">Contact Us</Link>
+          </div>
         </div>
         <div className="flex gap-3 ">
           <Link href="/login" className="px-4 py-2 bg-[#7B3F00] rounded-md text-white font-medium text-center">Login</Link>
