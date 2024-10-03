a57ca2cfb89bc56b058b3da8d5ca24ad16e014cf
tanvirtbf <tanver018765@gmail.com>
2024-10-03 17:45:40 +0600

    end

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -2,6 +2,9 @@ import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
+
+  const clsName = ``
+
   return (
     <div className="w-full h-20 bg-[#817e7e2b] px-5">
 
@@ -10,11 +13,11 @@ const Navbar = () => {
         <div className="">
           <h1 className="font-bold text-black text-3xl">ADSTERRA</h1>
         </div>
-        <div className="flex gap-2">
-          <Link className="w- font-bold text-black text-2xl" href="/">Home</Link>
-          <Link className="w- font-bold text-black text-2xl" href="/">About</Link>
-          <Link className="w- font-bold text-black text-2xl" href="/">Services</Link>
-          <Link className="w- font-bold text-black text-2xl" href="/">Contact Us</Link>
+        <div className="flex gap-2 h-full">
+          <Link className="bg-slate-400  h-full w- font-bold text-black text-2xl" href="/">Home</Link>
+          <Link className="bg-slate-400  h-full w- font-bold text-black text-2xl" href="/">About</Link>
+          <Link className="bg-slate-400  h-full w- font-bold text-black text-2xl" href="/">Services</Link>
+          <Link className="bg-slate-400  h-full w- font-bold text-black text-2xl" href="/">Contact Us</Link>
         </div>
         <div className="flex gap-3 ">
           <Link href="/login" className="px-4 py-2 bg-[#7B3F00] rounded-md text-white font-medium text-center">Login</Link>
