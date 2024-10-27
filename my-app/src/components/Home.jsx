ff48602ed066c80ed6bc01b4941f26ce5f0fa286
tanvirtbf <tanver018765@gmail.com>
2024-10-27 17:15:08 +0600

    Roboto font family import

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -9,45 +9,7 @@ export default function Home() {
 
   return (
     <div className="container px-5 text-white">
-      <div className="wrap flex">
-        <div className="left w-[20%]">
-          <div className="profile flex gap-2">
-            <div>
-              <Image src={UserImage} alt={UserImage} />
-            </div>
-            <div>
-              <h3>Tanvir Ahmed</h3>
-              <p>tanvir_tbf</p>
-            </div>
-          </div>
-
-          <div className="my-2">Navigation</div>
-
-          <div className="sidebar flex flex-col gap-3">
-            <div className="flex gap-2 cursor-pointer">
-              <div className="bg-[#23212C] p-2 rounded-full">
-                <MdOutlineDashboard className="text-white" />
-              </div>
-              <p>Dashboard</p>
-            </div>
-            <div className="flex gap-2 cursor-pointer">
-              <div className="bg-[#23212C] p-2 rounded-full">
-                <MdOutlineDashboard className="text-white" />
-              </div>
-              <p>Dashboard</p>
-            </div>
-            <div className="flex gap-2 cursor-pointer">
-              <div className="bg-[#23212C] p-2 rounded-full">
-                <MdOutlineDashboard className="text-white" />
-              </div>
-              <p>Dashboard</p>
-            </div>
-          </div>
-        </div>
-        <div className="right bg-black w-[80%]">
-          hello
-        </div>
-      </div>
+      Home Page
     </div>
   );
 }
