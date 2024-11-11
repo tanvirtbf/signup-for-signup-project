fa952db8845e72fd3ac8ed91a75f58bd681a46cd
tanvirtbf <tanver018765@gmail.com>
2024-11-11 22:21:47 +0600

    link changeJ

--- src/lib/services/auth.js
+++ src/lib/services/auth.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const authApi = createApi({
   reducerPath: 'authApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/api/user/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://tanvir-auth.onrender.com/api/user/' }),
   endpoints: (builder) => ({
     createUser: builder.mutation({
       query: (user) => {
